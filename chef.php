d1a2958d92986d25fd0b6a1f97c3c4db79301c12
Madhushani123 <kaushalyam234@gmail.com>
2024-03-19 19:11:39 +0530

    Our staff and Menu

---
+++ chef.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="chef.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="menu.php">Menu</a>
+</div>
+
+    <div class="div">
+        <h2 id="h2">Our Staff</h2>
+    </div>
+    
+    
+    <div class="row">
+    <div class="column">
+        <div class="card" style="width:300px;height:475px">
+        <img src="chef1.jpg" alt="koshila Dhananjaya" style="width:300px; height:300px;" >
+        <div class="container">
+            <h2>koshila Dhananjaya</h2>
+            <p class="title">CHEF</p>
+            <p>koshiladhananjaya@gmail.com</p>
+            <div style="margin: 24px 0;">
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+    
+    <div class="column">
+        <div class="card" style="width:300px;height:475px">
+        <img src="chef2.avif" alt=" SAMAN WIJERATNE" style="width:300px; height:300px;" class="saman">
+        <div class="container">
+            <h2>SAMAN WIJERATNE</h2>
+            <p class="title"> CHEF</p>
+            <p>samanwijerathne@gmail.com</p>
+            <div style="margin: 24px 0;">
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+    
+    <div class="column">
+        <div class="card" style="width:300px;height:475px">
+        <img src="chef3.jpg" alt="Kasun Bandara" style="width:300px; height:300px;">
+        <div class="container">
+            <h2>Kasun Bandara</h2>
+            <p class="title">CHEF</p>
+            <p>kasunbandara@example.com</p>
+            <div style="margin: 24px 0;">
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="column">
+        <div class="card" style="width:300px;height:475px">
+        <img src="chef4.jpg" alt="Peter Kuruvita" style="width:300px; height:300px;" class="peter">
+        <div class="container">
+            <h2>Peter Kuruvita</h2>
+            <p class="title">CHEF</p>
+            <p>peterkuruvita@example.com</p>
+            <div style="margin: 24px 0;">
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="column">
+        <div class="card" style="width:300px;height:475px">
+        <img src="chef5.jpg" alt="Chathurika" style="width:300px; height:300px;" class="chathu">
+        <div class="container">
+            <h2>Chathurika</h2>
+            <p class="title">CHEF</p>
+            <p>chathurika@example.com</p>
+            <div style="margin: 24px 0;">
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="column">
+        <div class="card" style="width:300px;height:475px">
+        <img src="chef6.jpg" alt="Gino D’Acampo" style="width:300px; height:300px;">
+        <div class="container">
+            <h2>Gino D’Acampo</h2>
+            <p class="title">CHEF</p>
+            <p>ginodacampo@example.com</p>
+            <div style="margin: 24px 0;">
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+            <p><button class="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+    </div>
+    
+
+</div>
+</body>
+</html>
